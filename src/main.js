9ca42aa4d5d45b69b8b40ac247ad9a4c52c32a2a
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-01 09:34:29 +0900

    add route page

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import Home from './components/Home.vue'
+import About from './components/About.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+createApp(App).use(router).mount('#app')
