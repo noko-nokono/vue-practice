76fa0d06ee3fa15f8148b4c7a5cbddf8c0f3c1c5
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-01 09:38:51 +0900

    add layout

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,28 @@ import { RouterView, RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <h1>Hello World</h1>
+  <header class="header">
+    <h1 class="red">Hello World</h1>
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/about">About</RouterLink>
     </nav>
   </header>
 
-  <main>
+  <main class="main">
     <RouterView />
   </main>
 </template>
 
 <style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 16px;
+}
+
+.main {
+  padding: 0 16px;
+}
 </style>
