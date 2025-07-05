926961a59c93f2f6de13d914ad9439754eac2587
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-05 10:10:27 +0900

    add counter

--- src/components/button.vue
+++ src/components/button.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+defineProps<{increment: () => void}>();
+</script>
+
 <template>
-  <button class="button">ボタン</button>
+  <button class="button" @click="increment">ボタン</button>
 </template>
--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import Button from '@/components/button.vue'
+
+const count = ref(0)
+
+const increment = () => {
+  count.value++
+}
 </script>
 
 <template>
   <h1>Home Page</h1>
-  <Button />
+  <p>{{ count }}</p>
+  <Button :increment="increment" />
 </template>
