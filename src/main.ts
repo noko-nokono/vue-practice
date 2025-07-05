3c2e987d0c40bd2339e648fcb8803ecf86422cbf
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-05 10:51:05 +0900

    fix createWebHistory

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createWebHistory, createRouter } from 'vue-router'
 import Home from './page/Home.vue'
 import Counter from './page/Counter.vue'
-import About from './page/About.vue'
+import Todo from './page/Todo.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/counter', component: Counter },
-  { path: '/about', component: About },
+  { path: '/todo', component: Todo },
 ]
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 })
 
