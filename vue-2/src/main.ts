0c913d7ebe01345fa77a97786bf7f3bd1631fafb
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-09 09:31:32 +0900

    refactor setting

--- vue-2/src/main.ts
+++ vue-2/src/main.ts
@@ -1,29 +1,7 @@
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue';
-import vuetify from './vuetify';
-import Home from './page/Home.vue';
-import Counter from './page/Counter.vue';
-
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/counter',
-    name: 'Counter',
-    component: Counter
-  }
-];
-
-const router = new VueRouter({
-  mode: 'history',
-  routes
-});
+import vuetify from './settings/vuetify';
+import router from './settings/routing';
 
 new Vue({
   router,
---
+++ vue-2/src/settings/routing.ts
@@ -0,0 +1,24 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../page/Home.vue';
+import Counter from '../page/Counter.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/counter',
+    name: 'Counter',
+    component: Counter
+  }
+];
+
+export default new VueRouter({
+  mode: 'history',
+  routes
+});
