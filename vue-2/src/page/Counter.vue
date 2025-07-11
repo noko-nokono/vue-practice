4bcff8338cfda29bee33b6dbae076b9f09eac097
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-11 15:18:18 +0900

    add counter

--- vue-2/src/page/Counter.vue
+++ vue-2/src/page/Counter.vue
@@ -1,6 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    decriment() {
+      this.count--;
+    }
+  }
+}
+</script>
+
 <template>
   <div>
     <h1>Counter</h1>
+    <div class="counter">
+      <p>{{ count }}</p>
+      <v-btn @click="increment">Increment</v-btn>
+      <v-btn @click="decriment">Decriment</v-btn>
+    </div>
     <v-btn color="secondary" to="/">Back to Home</v-btn>
   </div>
 </template>
+
+<style>
+.counter {
+  margin-bottom: 30px;
+}
+</style>
