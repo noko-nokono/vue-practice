79757135d156b751e824312cb8114dd677e7728f
山﨑 孝二 <kouzi619soccer@icloud.com>
2025-07-14 15:31:28 +0900

    add data fetch

---
+++ vue-2/src/page/DataFetch.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  data() {
+    return {
+      data: null,
+      loading: false,
+    }
+  },
+  async mounted() {
+    this.loading = true;
+    const url = "https://pokeapi.co/api/v2/pokemon/ditto";
+    const response = await fetch(url).then((res) => {
+      this.loading = false;
+      return res.json();
+    })
+    this.data = response;
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div>DataFetch</div>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <img :src="data.sprites.front_default" />
+      <div>{{ data.species.name }}</div>
+    </div>
+</div>
+</template>
